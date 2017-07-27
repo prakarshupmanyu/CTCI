9a285b1fcdc63ae23dd335224651c6e778daf3db
Prakarsh Upmanyu <upmanyu@usc.edu>
2017-07-27 10:43:16 -0700

    adding q3 solution for linked list

--- Ch-2-Linked_lists/q3.java
+++ Ch-2-Linked_lists/q3.java
@@ -27,6 +27,19 @@ public class q3{
 		}
 	}
 
+	//I didn't read the question properly. So we are only given the node to delete and not the HEAD
+	//So we cannot figure out if the given node is head or not. But if the given node is end of list then we will display an appropriate message
+	private boolean bookSolution(node mid){
+		if (mid == null || mid.next == null){
+			System.out.println("The given node is the end of list. It cannot be deleted.");
+			return false;
+		}
+		node n = mid.next;
+		mid.data = n.data;
+		mid.next = n.next;
+		return true;
+	}
+
 	public static void main(String args[]){
 		
 		ll llo = new ll();
